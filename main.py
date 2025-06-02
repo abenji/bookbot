bfc6ad803c40996b721e3abdf2a54836d8aa04aa
abenji <adbenjamin3@gmail.com>
2025-06-02 10:58:19 -0500

    create character list sort function; update report printing formatting on main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from stats import get_word_count, get_book_text, get_character_count
+from stats import get_word_count, get_book_text, get_character_count, sort_character_list
 book_path = "books/frankenstein.txt"
 
 def main():    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_word_count(get_book_text(book_path))} total words")
+    print("--------- Character Count -------")
 
-    print(f"{get_word_count(get_book_text(book_path))} words found in the document")
-    print(get_character_count(get_book_text(book_path)))
-    
+    for char, count in (sort_character_list(get_character_count(get_book_text(book_path)))):
+        if char.isalpha() == True:
+            print(f"{char}: {count}")
+    #print(sort_character_list(get_character_count(get_book_text(book_path))))
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -21,4 +21,20 @@ def get_character_count(book_text):
             character_dict[char] = 1
     return character_dict
 
+def sort_character_list(character_dict):
+    character_list = []
+
+    for char, count in character_dict.items():
+        character_list.append((char,count))
+
+    def sort_on(item):
+        return item[1]
+    
+    character_list.sort(reverse=True, key=sort_on)
+
+    return character_list
+
+
+
+
 
