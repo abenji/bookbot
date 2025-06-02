b90f414d6dd842a3838bce7aaa9bc6b070f8ccac
abenji <adbenjamin3@gmail.com>
2025-06-02 13:33:51 -0500

    remove hard-coded book path in main; add error handling for missing book path

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from stats import get_word_count, get_book_text, get_character_count, sort_character_list
-book_path = "books/frankenstein.txt"
+import sys
 
-def main():    
+try:
+    book_path = sys.argv[1]
+except:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+def main():
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {get_word_count(get_book_text(book_path))} total words")
     print("--------- Character Count -------")
@@ -11,6 +17,7 @@ def main():
     for char, count in (sort_character_list(get_character_count(get_book_text(book_path)))):
         if char.isalpha() == True:
             print(f"{char}: {count}")
-    #print(sort_character_list(get_character_count(get_book_text(book_path))))
+
     print("============= END ===============")
+
 main()
